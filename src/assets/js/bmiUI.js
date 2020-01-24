9e7cad1bb705304e089d8ff2afcef0aef0bef092
Janko <mailodjanka@gmail.com>
2020-01-24 12:11:25 +0100

    Added BMI classification function

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,8 +2,9 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let  results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let message =getBMIClassification(results)
     let displayResultsElement = document.getElementById('results')
-    displayResultsElement.innerHTML = `<h2>Your BMI value is: ${results}</h2>`
+    displayResultsElement.innerHTML = `<h2>Your BMI value is: ${results} and you are ${message}!</h2>`
    
     
 }
